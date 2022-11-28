7110b773369f27b6ad6da09de2766487d18e57b9
kazi331 <kazisharifulislam52@gmail.com>
2022-11-28 11:45:14 +0600

    views added in login and signup

--- app.js
+++ app.js
@@ -14,14 +14,13 @@ app.use(express.json())
 app.set('view engine', 'ejs');
 
 // databasee connection
-
 const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.oxxl7li.mongodb.net`
 
-mongoose.connect(dbURI)
+mongoose.connect(dbURI, { dbName: 'jwt-auth' })
   .then(() => app.listen(3000, () => console.log('DB Connected')))
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('sm`oothies'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(router);
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -15,6 +16,22 @@ const userSchema = new mongoose.Schema({
     minLength: [6, 'Password must be at least 6 characters long'],
   },
 })
+
+// mongoose hooks before and after
+// Fire a function after doc saved in the db - optional
+userSchema.post('save', (doc, next) => {
+  console.log('new user created', doc)
+  next();
+})
+
+// Fire a function before doc saved in the db - optional
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt); // hash the password and assign with the user object
+  next();
+})
+
+
 
 const User = mongoose.model('user', userSchema)
 
